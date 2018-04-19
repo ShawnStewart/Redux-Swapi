53c579ee8dbf77ef8fa94e655542c6b2f8681124
Shawn Stewart <s.stewart0220@gmail.com>
2018-04-19 12:22:22 -0500

    Completion of redux swapi

--- src/actions/index.js
+++ src/actions/index.js
@@ -7,3 +7,25 @@
 // we'll have to be sure to make our promise resolve within our new "thunk based middlware"
 // the url to fetch charicters from is `https://swapi.co/api/people/`
 // remember that now we have controll over our thunk-based
+
+import axios from 'axios';
+
+
+export const FETCHING_CHARS = 'FETCHING_CHARS';
+export const ERROR_FETCHING_CHARS = 'ERROR_FETCHING_CHARS';
+export const CHARS_FETCHED = 'CHARS_FETCHED';
+
+export const fetchChars = () => {
+	const starWarsChars = axios.get(`https://swapi.co/api/people/`)
+	return dispatch => {
+		dispatch({ type: FETCHING_CHARS })
+		starWarsChars
+			.then(response => {
+				console.log('response', response);
+				dispatch({ type: CHARS_FETCHED, payload: response.data.results })
+			})
+			.catch(err => {
+				dispatch({ type: ERROR_FETCHING_CHARS, payload: 'Error Fetching Chars' })
+			})
+	}
+}
